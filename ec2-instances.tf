30d39fd9eb66fb9534eb4646d44e1a787f71c8cb
Rufus Ajiboye <ruufman@gmail.com>
2023-09-06 15:44:17 +0100

    File Updated

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -57,4 +57,19 @@ resource "aws_instance" "appnode4" {
 }
 
 
+resource "aws_instance" "appnode5" {
+  
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  key_name      = var.ec2_key_name
+  subnet_id = aws_subnet.subnet_public1.id
+  vpc_security_group_ids = [aws_security_group.myhrapp_sg.id] 
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "HrApp_Server"
+  }
+}
+
+
 
